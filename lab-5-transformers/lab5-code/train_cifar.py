fed3b7fa31b29789b328237928f3608f32da6146
MoiseGuran13 <60397889+MoiseGuran13@users.noreply.github.com>
2023-11-16 18:40:23 +0000

    Lab 5 checkpoint

--- lab-5-transformers/lab5-code/train_cifar.py
+++ lab-5-transformers/lab5-code/train_cifar.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
-from __future__ import annotations
-
 import time
 from multiprocessing import cpu_count
 from typing import Union, NamedTuple
 
 import torch
 import torch.backends.cudnn
+import torchvision
 import numpy as np
 from torch import nn, optim
-from torch.nn import functional as F
+from torch.nn import functional as f
 import torchvision.datasets
 from torch.optim.optimizer import Optimizer
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from torchvision import transforms
-
+from torchvision.transforms import RandomHorizontalFlip
 
 import argparse
 from pathlib import Path
@@ -23,14 +22,13 @@ from pathlib import Path
 torch.backends.cudnn.benchmark = True
 
 parser = argparse.ArgumentParser(
-    description="Train a simple Transformer on CIFAR-10",
+    description="Train a simple CNN on CIFAR-10",
     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
 )
 default_dataset_dir = Path.home() / ".cache" / "torch" / "datasets"
 parser.add_argument("--dataset-root", default=default_dataset_dir)
 parser.add_argument("--log-dir", default=Path("logs"), type=Path)
 parser.add_argument("--learning-rate", default=1e-2, type=float, help="Learning rate")
-parser.add_argument("--num-heads", default=1, type=int, help="Number of Heads")
 parser.add_argument(
     "--batch-size",
     default=128,
@@ -68,6 +66,11 @@ parser.add_argument(
     type=int,
     help="Number of worker processes used to load data.",
 )
+# LAB 4:
+parser.add_argument("--data-aug-hflip", action="store_true", dest="isHFlip")
+parser.add_argument("--data-aug-brightness", default=0, type=float, dest="brightness")
+parser.add_argument("--data-aug-reflection", default=0, type=int, dest="reflectionPadding")
+parser.add_argument("--dropout", default=0, type=float)
 
 
 class ImageShape(NamedTuple):
@@ -83,13 +86,21 @@ else:
 
 
 def main(args):
-    transform = transforms.ToTensor()
+    augmentations = []
+    if args.isHFlip:
+        augmentations.append(transforms.RandomHorizontalFlip())
+    if args.brightness > 0:
+        augmentations.append(transforms.ColorJitter(brightness=args.brightness))
+    if args.reflectionPadding > 0:
+        augmentations.append(transforms.Pad(padding=args.reflectionPadding, padding_mode='reflect'))
+    augmentations.append(transforms.ToTensor())
+    transform = transforms.Compose(augmentations)
     args.dataset_root.mkdir(parents=True, exist_ok=True)
     train_dataset = torchvision.datasets.CIFAR10(
         args.dataset_root, train=True, download=True, transform=transform
     )
     test_dataset = torchvision.datasets.CIFAR10(
-        args.dataset_root, train=False, download=False, transform=transform
+        args.dataset_root, train=False, download=False, transform=transforms.ToTensor()
     )
     train_loader = torch.utils.data.DataLoader(
         train_dataset,
@@ -106,17 +117,19 @@ def main(args):
         pin_memory=True,
     )
 
-    model = CIFAR_Transformer(height=32, width=32, channels=3, patch_size=(8, 8), class_count=10, n_heads=args.num_heads)
+    model = CNN(height=32, width=32, channels=3, class_count=10, dropout=args.dropout)
 
+    # TASK 8: Redefine the criterion to be softmax cross entropy
     criterion = nn.CrossEntropyLoss()
 
-    optimizer = optim.SGD(model.parameters(), lr=0.05)
+    # TASK 11: Define the optimizer
+    optimizer = torch.optim.SGD(model.parameters(), args.learning_rate, momentum=0.9)
 
     log_dir = get_summary_writer_log_dir(args, count_parameters(model))
     print(f"Writing logs to {log_dir}")
     summary_writer = SummaryWriter(
-            str(log_dir),
-            flush_secs=5
+        str(log_dir),
+        flush_secs=5
     )
     trainer = Trainer(
         model, train_loader, test_loader, criterion, optimizer, summary_writer, DEVICE
@@ -132,41 +145,70 @@ def main(args):
     summary_writer.close()
 
 
-class CIFAR_Transformer(nn.Module):
-    def __init__(self, height: int, width: int, channels: int, patch_size: tuple[int, int], class_count: int, n_heads: int=1):
+class CNN(nn.Module):
+    def __init__(self, height: int, width: int, channels: int, class_count: int, dropout: float):
         super().__init__()
-        assert width % patch_size[0] == 0 and height % patch_size[1] == 0
-        assert n_heads > 1
+        self.input_shape = ImageShape(height=height, width=width, channels=channels)
         self.class_count = class_count
-        self.n_heads = n_heads
-        #TASKS 2, 3.1, 7, 8, 9.1, 11.1
-        self.patch_size = patch_size
-        self.transformerEncoderLayer1 = nn.TransformerEncoderLayer(
-            d_model = patch_size[0] * patch_size[1] * channels,
-            nhead = n_heads,
-            batch_first = True
-        )
-        self.transformerEncoderLayer2 = nn.TransformerEncoderLayer(
-            d_model=patch_size[0] * patch_size[1] * channels,
-            nhead=n_heads,
-            batch_first=True
-        )
-        self.fc1 = nn.Linear(patch_size[0] * patch_size[1] * channels, 100)
-        self.fc2 = nn.Linear(100, 10)
-        self.cls = nn.Parameter(torch.randn(patch_size[0] * patch_size[1] * channels))
 
+        self.conv1 = nn.Conv2d(
+            in_channels=self.input_shape.channels,
+            out_channels=32,
+            kernel_size=(5, 5),
+            padding=(2, 2),
+        )
+        self.initialise_layer(self.conv1)
+        self.batch1 = nn.BatchNorm2d(num_features=32)
+
+        self.pool1 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+        # TASK 2-1: Define the second convolutional layer and initialise its parameters
+        self.conv2 = nn.Conv2d(
+            in_channels=32,
+            out_channels=64,
+            kernel_size=(5, 5),
+            padding=(2, 2),
+        )
+        self.initialise_layer(self.conv2)
+        self.batch2 = nn.BatchNorm2d(num_features=64)
+
+        # TASK 3-1: Define the second pooling layer
+        self.pool2 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+        # TASK 5-1: Define the first FC layer and initialise its parameters
+        self.fc1 = nn.Linear(4096, 1024)
+        self.batchFc = nn.BatchNorm1d(num_features=1024)
+        self.initialise_layer(self.fc1)
+        # self.initialise_layer(self.batch1)
+        # TASK 6-1: Define the last FC layer and initialise its parameters
+        self.fc2 = nn.Linear(1024, 10)
+        self.initialise_layer(self.fc2)
+        # self.initialise_layer(self.batch2)
+
+        # Lab 4
+        self.dropout = nn.Dropout(p=dropout)
 
     def forward(self, images: torch.Tensor) -> torch.Tensor:
-        # x = nn.Identity(images)
-        #TASKS 1, 2, 3.2, 7, 8, 9.2, 10, 11.2
-        x = patchify(images, self.patch_size[0], self.patch_size[1])
-        # x = x  self.cls
-        x = self.transformerEncoderLayer1(x)
-        x = self.transformerEncoderLayer2(x)
-        x = x.mean(1)
-        x = self.fc1(x)
-        x = self.fc2(x)
+        x = f.relu(self.batch1(self.conv1(images)))
+        x = self.pool1(x)
+        # TASK 2-2: Pass x through the second convolutional layer
+        x = f.relu(self.batch2(self.conv2(x)))
+        # TASK 3-2: Pass x through the second pooling layer
+        x = self.pool2(x)
+        # TASK 4: Flatten the output of the pooling layer, so it is of shape
+        #         (batch_size, 4096)
+        x = torch.flatten(x, start_dim=1)
+        # TASK 5-2: Pass x through the first fully connected layer
+        x = self.fc1(self.dropout(x))
+        x = self.batchFc(x)
+        # TASK 6-2: Pass x through the last fully connected layer
+        x = self.fc2(self.dropout(x))
         return x
+
+    @staticmethod
+    def initialise_layer(layer):
+        if hasattr(layer, "bias"):
+            nn.init.zeros_(layer.bias)
+        if hasattr(layer, "weight"):
+            nn.init.kaiming_normal_(layer.weight)
 
 
 class Trainer:
@@ -198,7 +240,6 @@ class Trainer:
         start_epoch: int = 0
     ):
         self.model.train()
-        #TASK 5
         for epoch in range(start_epoch, epochs):
             self.model.train()
             data_load_start_time = time.time()
@@ -207,14 +248,21 @@ class Trainer:
                 labels = labels.to(self.device)
                 data_load_end_time = time.time()
 
-
+                # TASK 1: Compute the forward pass of the model, print the output shape
+                #         and quit the program
                 logits = self.model.forward(batch)
 
-                #TASK 4
+                # TASK 7: Rename `output` to `logits`, remove the output shape printing
+                #         and get rid of the `import sys; sys.exit(1)`
+
+                # TASK 9: Compute the loss using self.criterion and
+                #         store it in a variable called `loss`
                 loss = self.criterion(logits, labels)
 
+                # TASK 10: Compute the backward pass
                 loss.backward()
 
+                # TASK 12: Step the optimizer and then zero out the gradient buffers.
                 self.optimizer.step()
                 self.optimizer.zero_grad()
 
@@ -235,7 +283,6 @@ class Trainer:
             self.summary_writer.add_scalar("epoch", epoch, self.step)
             if ((epoch + 1) % val_frequency) == 0:
                 self.validate()
-
                 # self.validate() will put the model in validation mode,
                 # so we have to switch back to train mode afterwards
                 self.model.train()
@@ -288,14 +335,17 @@ class Trainer:
                 results["preds"].extend(list(preds))
                 results["labels"].extend(list(labels.cpu().numpy()))
 
-        accuracy = compute_accuracy(
+        accuracy = compute_per_class_accuracy(
             np.array(results["labels"]), np.array(results["preds"])
         )
+        per_class_accuracy = [
+           f"{i}:{accuracy[i]}" for i in range(len(accuracy))
+        ]
         average_loss = total_loss / len(self.val_loader)
 
         self.summary_writer.add_scalars(
                 "accuracy",
-                {"test": accuracy},
+                {"test": accuracy.sum()/len(accuracy)},
                 self.step
         )
         self.summary_writer.add_scalars(
@@ -303,7 +353,7 @@ class Trainer:
                 {"test": average_loss},
                 self.step
         )
-        print(f"validation loss: {average_loss:.5f}, accuracy: {accuracy * 100:2.2f}")
+        print(f"validation loss: {average_loss:.5f}, accuracy: {per_class_accuracy}")
 
 
 def compute_accuracy(
@@ -316,6 +366,19 @@ def compute_accuracy(
     """
     assert len(labels) == len(preds)
     return float((labels == preds).sum()) / len(labels)
+
+
+def compute_per_class_accuracy(
+    labels: Union[torch.Tensor, np.ndarray], preds: Union[torch.Tensor, np.ndarray]
+) -> [float]:
+    assert len(labels) == len(preds)
+    classes = np.zeros(len(np.unique(labels)))
+    f_v = np.zeros(len(classes))
+    for i in range(len(labels)):
+        f_v[labels[i]] += 1
+        if labels[i] == preds[i]:
+            classes[labels[i]] += 1
+    return classes/f_v
 
 
 def get_summary_writer_log_dir(args: argparse.Namespace, n_parameters: int) -> str:
@@ -331,13 +394,18 @@ def get_summary_writer_log_dir(args: argparse.Namespace, n_parameters: int) -> s
         untangle in TB).
     """
     tb_log_dir_prefix = (
-        f'Transformer_params={n_parameters}'
-        f'_bs={args.batch_size}'
+        f'CNN_parameters={n_parameters}'
+        f'_bn_bs={args.batch_size}'
+        f'_dropout={args.dropout}'
         f'_lr={args.learning_rate}'
-        f'_heads={args.num_heads}'
-        f'_run_'
-    )
+        f'_momentum=0.9'
+        f"_brightness={args.brightness}" +
+        ("_hflip" if args.isHFlip else "") +
+        (f"_reflect={args.reflectionPadding}" if args.reflectionPadding > 0 else "") +
+        f'_run_')
+
     i = 0
+    tb_log_dir = args.log_dir / (tb_log_dir_prefix + str(i))
     while i < 1000:
         tb_log_dir = args.log_dir / (tb_log_dir_prefix + str(i))
         if not tb_log_dir.exists():
@@ -350,24 +418,5 @@ def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
 
-def patchify(batch: torch.Tensor, x_size: int, y_size: int):
-    unfold = nn.Unfold(kernel_size=(x_size, y_size), stride=x_size)
-    return unfold(batch).permute(0, 2, 1)
-
-
 if __name__ == "__main__":
-    # main(parser.parse_args())
-
-    input = torch.randn(10, 4, 192)
-    print(input)
-    # layer = nn.TransformerEncoderLayer(
-    #         d_model = 8*8*3,
-    #         nhead = 1,
-    #         batch_first = True
-    #     )
-    parameter = nn.Parameter(torch.randn(8*8*3)).expand(1, 1, 8*8*3)
-    print(parameter)
-    # print(torch.cat((parameter, input), (0, 1)).shape)
-    # print(layer(input).shape)
-
-# HW: Pytorch Transformer layer
+    main(parser.parse_args())
